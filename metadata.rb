2290a0f60a2910b8fe9d762ce55edf93a6587bd5
Matt Ray <github@mattray.dev>
2021-05-27 13:21:27 +1000

    bump for new functionality and testing

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Matt Ray'
 maintainer_email 'matt@chef.io'
 license 'Apache-2.0'
 description 'Schedules a run list for running within a time window'
-version '0.1.0'
+version '0.2.0'
 chef_version '>= 15.0'
 
 issues_url       'https://github.com/mattray/scheduled_run_list-cookbook/issues'
