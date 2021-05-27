42bffa1144ed066ef541aaf8c18733f24ae9e701
Matt Ray <github@mattray.dev>
2021-05-27 13:21:01 +1000

    add attribute for test detection

--- test/integration/test_cookbook/recipes/run_list.rb
+++ test/integration/test_cookbook/recipes/run_list.rb
@@ -1,1 +1,3 @@
 log 'THIS IS THE RUN LIST RECIPE. THIS IS INSIDE THE WINDOW.'
+
+node.default['test_cookbook']['run_list'] = true
--- test/integration/test_cookbook/recipes/run_list2.rb
+++ test/integration/test_cookbook/recipes/run_list2.rb
@@ -1,1 +1,3 @@
 log 'THIS IS THE RUN LIST 2 RECIPE.'
+
+node.default['test_cookbook']['run_list2'] = true
